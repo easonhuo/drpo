2dfee0947c977d3206c52b7085b8275d73630663
easonhuo <huoyusen1@126.com>
2026-07-17 21:37:42 +0800

    PAPER-CODE-REFERENCE-01 export C-U1 mechanism API

--- paper_code/src/drpo_reference/continuous/__init__.py
+++ paper_code/src/drpo_reference/continuous/__init__.py
@@ -13,6 +13,15 @@ from .cu1 import (
     near_far_losses,
     positive_loss,
 )
+from .cu1_mechanism import (
+    CausalRun,
+    CU1CausalProtocol,
+    CU1SourceProtocol,
+    intervention_gradients,
+    per_sample_negative_gradient,
+    run_causal_intervention,
+    source_diagnostic,
+)
 from .cu1_training import (
     CU1PositiveProtocol,
     PositiveRun,
@@ -27,8 +36,11 @@ from .gaussian import (
 )
 
 __all__ = [
+    "CU1CausalProtocol",
     "CU1PositiveProtocol",
     "CU1Protocol",
+    "CU1SourceProtocol",
+    "CausalRun",
     "Environment",
     "GaussianActor",
     "PositiveRun",
@@ -38,11 +50,15 @@ __all__ = [
     "evaluation",
     "gaussian_log_prob",
     "gaussian_output_components",
+    "intervention_gradients",
     "local_negative_loss",
     "make_actor",
     "make_environment",
     "near_far_losses",
+    "per_sample_negative_gradient",
     "positive_loss",
+    "run_causal_intervention",
+    "source_diagnostic",
     "standardized_distance",
     "train_positive",
     "write_positive_run",
