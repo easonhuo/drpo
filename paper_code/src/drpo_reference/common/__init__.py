43fa22043f2ba44222debb63f7b476965f1a647a
easonhuo <huoyusen1@126.com>
2026-07-17 20:54:32 +0800

    PAPER-CODE-REFERENCE-01 export shared runtime utilities

--- paper_code/src/drpo_reference/common/__init__.py
+++ paper_code/src/drpo_reference/common/__init__.py
@@ -1,5 +1,16 @@
-"""Shared records used by all paper experiments."""
+"""Shared records and deterministic utilities used by paper experiments."""
 
 from .events import EventFlags, EventKind, EventRecord
+from .io import atomic_json, read_csv, write_csv
+from .seeding import cpu_generator, seed_all
 
-__all__ = ["EventFlags", "EventKind", "EventRecord"]
+__all__ = [
+    "EventFlags",
+    "EventKind",
+    "EventRecord",
+    "atomic_json",
+    "cpu_generator",
+    "read_csv",
+    "seed_all",
+    "write_csv",
+]
